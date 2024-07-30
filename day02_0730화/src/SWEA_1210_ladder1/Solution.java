c47071738845101882f63308a821119fc68df8be
Huk9uri <huk9uri@gmail.com>
2024-07-30 17:43:22 +0900

    0730

--- day02_0730화/src/SWEA_1210_ladder1/Solution.java
+++ day02_0730화/src/SWEA_1210_ladder1/Solution.java
@@ -1,5 +1,40 @@
 package SWEA_1210_ladder1;
 
-public class Solution {
+import java.util.Scanner;
 
+public class Solution {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		for (int t = 1; t <= 10; t++) {
+			int testCase = sc.nextInt();
+			// 사다리 초기화
+			int[][] ladder = new int[100][100];
+			int x = -1; // 도착지점의 좌표는 (99,x)이다.
+			// 사다리에 값을 넣고
+			for (int i = 0; i < ladder.length; i++) {
+				for (int j = 0; j < ladder.length; j++) {
+					ladder[i][j] = sc.nextInt();
+				}
+			}
+			// 가장 아래줄 i=99 에서 2의 j 좌표를 찾고
+			for (int j = 0; j < ladder.length; j++) {
+				if (ladder[99][j] == 2) {
+					x = j; //
+				}
+			}
+			for (int i = 99; i >= 0; i--) {
+				if(x>0 && ladder[i][x-1] == 1) { // 왼쪽에 1이 있다? 그럼 왼쪽으로 1 탐색 시작
+					while(x>0 && ladder[i][x-1] ==1) { // 배열 범위 안에서 0이 나올때까지
+						x--; // 1칸씩 왼쪽으로 이동한다. 
+					}
+				}else if(x<99 && ladder[i][x+1] == 1) {
+					while(x<99 && ladder[i][x+1] == 1) {
+						x++;
+					}
+				}
+			}
+			   System.out.println("#" + testCase + " " + x);
+		}
+	}
 }
--- day02_0730화/src/SWEA_1954_달팽이숫자/Solution.java
+++ day02_0730화/src/SWEA_1954_달팽이숫자/Solution.java
@@ -1,5 +1,48 @@
 package SWEA_1954_달팽이숫자;
 
-public class Solution {
+import java.util.Arrays;
+import java.util.Scanner;
 
+public class Solution {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int testCase = sc.nextInt();
+
+		for (int i = 1; i <= testCase; i++) {
+			int n = sc.nextInt();
+			int[][] map = new int[n][n];
+
+			int x = 0;
+			int y = 0;
+			int direction = 0;
+			// 우 : 0 하 : 1 좌 : 2 상 : 3
+			int[] dx = { 0, 1, 0, -1 };
+			int[] dy = { 1, 0, -1, 0 };
+
+
+			int num = 1;
+			while (num <= n * n) {
+				map[x][y] = num;
+				num++;
+
+				int nx = x + dx[direction];
+				int ny = y + dy[direction];
+				if (nx >= 0 && nx < n && ny >= 0 && ny < n && map[nx][ny] == 0) {
+					x = nx;
+					y = ny;
+				} else {
+					direction = (direction + 1) % 4;
+					x = x + dx[direction];
+					y = y + dy[direction];
+				}
+			}
+			System.out.println("#" + i);
+			for (int j = 0; j < n; j++) {
+				for (int k = 0; k < n; k++) {
+					System.out.print(map[j][k] + " ");
+				}
+				System.out.println();
+			}
+		}
+	}
 }
