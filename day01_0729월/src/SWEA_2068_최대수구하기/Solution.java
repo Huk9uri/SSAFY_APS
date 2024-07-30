56c87e9c790d31d5c656ebca1baa726790d80c16
Huk9uri <huk9uri@gmail.com>
2024-07-30 12:57:18 +0900

    SWEA_2068

--- day01_0729월/src/SWEA_2068_최대수구하기/Solution.java
+++ day01_0729월/src/SWEA_2068_최대수구하기/Solution.java
@@ -1,5 +1,25 @@
 package SWEA_2068_최대수구하기;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Solution {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+
+		int[] arr = new int[10];
+
+		for (int i = 1; i <= t; i++) {
+			for (int j = 0; j < arr.length; j++) {
+				arr[j] = sc.nextInt();
+			}
+			Arrays.sort(arr);
+
+			System.out.println("#" + i + " " + arr[arr.length - 1]);
+		}
+	}
 
 }
