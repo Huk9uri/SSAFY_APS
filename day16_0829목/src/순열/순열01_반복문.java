25262d347ab4a559c1b0b46f4a232a829c62fa98
Huk9uri <huk9uri@gmail.com>
2024-08-29 12:00:58 +0900

    0829

---
+++ day16_0829목/src/순열/순열01_반복문.java
@@ -0,0 +1,21 @@
+package 순열;
+
+public class 순열01_반복문 {
+	static int[] nums;
+	static int N;
+
+	public static void main(String[] args) {
+		nums = new int[] { 1, 2, 3 };
+		N = nums.length;
+
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				if (i != j)
+					for (int k = 0; k < N; k++) {
+						if (i != k && j != k)
+							System.out.println(nums[i] + " " + nums[j] + " " + nums[k]);
+					}
+			}
+		}
+	}
+}
